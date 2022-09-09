f18a44161fb1b417eaaecfbc962acc230a936a20
boluwalekan <boluwadelekon@yahoo.com>
2022-09-08 19:59:06 -0700

    Hello, puts new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,15 +4,11 @@
 
 /**
 
-* main - print the string in the put function
+* main - Entry point
 
 *
 
-* Description: using the main function
-
-* this program prints "Programming is like building a multilingual puzzle
-
-* Return: 0
+* Return: Always 0 (Success)
 
 */
 
@@ -20,8 +16,12 @@ int main(void)
 
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+return (0);
 
 }
+
+
+
+
