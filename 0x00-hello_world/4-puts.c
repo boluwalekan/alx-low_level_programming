5de4f26ea9ec37a2f9086bb7c4625ce374c9e742
boluwalekan <boluwadelekon@yahoo.com>
2022-09-11 19:05:47 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include >studio.h>
+#include <stdio.h>
 
 /**
  * main - prints the phrase "Programming is like building a multilingual puzzle
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}       
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
