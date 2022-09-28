bfe04a47cb0859e9e39dff141c296d67886d19ec
boluwalekan <boluwadelekon@yahoo.com>
2022-09-28 16:34:05 -0700

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,7 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
 
 int _putchar(char c);
 void _puts_recursion(char *s);
@@ -8,8 +10,11 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int is_sqrt(int n, int square);
 int is_prime_number(int n);
+int is_prime(int n, int i);
 int is_palindrome(char *s);
+int palindrome_check(char *s, int len, int i);
 int wildcmp(char *s1, char *s2);
 
-#endif
+#endif /* MAIN_H */
