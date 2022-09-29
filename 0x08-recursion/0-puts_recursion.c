40b1ef5e6bb0d02b08c2ecc6a88724019e41da1f
boluwalekan <boluwadelekon@yahoo.com>
2022-09-28 20:49:19 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings and returns 1 if identical
- * @s1: string to be checked
- * @s2: pattern to be used
+ * _puts_recursion - a function that prints a new line
  *
- * Return: 1 if identical, 0 otherwise
- */
-int wildcmp(char *s1, char *s2)
+ * @s: the string
+ *
+ * Return: nothing
+*/
+
+void _puts_recursion(char *s)
 {
-	if (*s2 == '\0')
-		return (*s1 == '\0');
-	if (*s2 == *s1)
-		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
-	return (0);
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
